64c9a253463b8aee506e21f4c46828adfb3704ef
Venusian26 <juanfigueroc26@gmail.com>
2018-11-01 22:52:12 -0700

    CRUD CLIENTES, CRUD PRODUCTOS, IMPLEMENTADO BOOSTRAP4

--- AppRails/config/routes.rb
+++ AppRails/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'principal#index'
+  resources :productos
+  resources :clientes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- AppRails/db/schema.rb
+++ AppRails/db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_02_050247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clientes", force: :cascade do |t|
+    t.string "nombre"
+    t.string "rfc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "productos", force: :cascade do |t|
+    t.string "name"
+    t.decimal "precio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
